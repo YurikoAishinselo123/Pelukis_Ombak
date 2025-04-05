657e0cb198fb770dfaf7cfaa7b3f3d2787542644
YurikoAishinselo123 <aishinseloyuriko124@gmail.com>
2025-04-05 21:00:37 +0700

    +Mission UI (Not Fixed) +Mission System

---
+++ Assets/Script/UI/MissionUI.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class MissionUI : MonoBehaviour
+{
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI progressText;
+
+    public void Init(Mission mission, int progress = 0)
+    {
+        if (mission == null)
+        {
+            Debug.LogWarning("Mission is null!");
+            return;
+        }
+
+        titleText.text = mission.title;
+        descriptionText.text = mission.description;
+
+        int max = mission.qty ?? 1;
+        progressText.text = $"{progress}/{max}";
+    }
+}
