d01065b2fc42dc6f9ee14787dc733d5a7b19439b
YurikoAishinselo123 <aishinseloyuriko124@gmail.com>
2025-05-06 19:57:34 +0700

    Pause Menu

---
+++ Assets/Script/UI/PauseUI.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseUI : MonoBehaviour
+{
+    public static PauseUI Instance;
+    public GameObject PauseCanvas;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button quitButton;
+    private bool isPaused = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        resumeButton.onClick.AddListener(ResumeGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    void Update()
+    {
+        if (InputManager.Instance.Back)
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        isPaused = true;
+        PauseCanvas.SetActive(true);
+    }
+
+    private void ResumeGame()
+    {
+        isPaused = false;
+        PauseCanvas.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        SceneLoader.Instance.LoadMainMenu();
+    }
+}
