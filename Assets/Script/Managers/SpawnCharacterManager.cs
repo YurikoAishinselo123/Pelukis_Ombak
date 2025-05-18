a1780e2990629bb675fbe8a23e1ad9c7150646fa
Denifu <denifu102@gmail.com>
2025-05-19 00:21:03 +0700

    fixReplayFromMainMenu

--- Assets/Script/Managers/SpawnCharacterManager.cs
+++ Assets/Script/Managers/SpawnCharacterManager.cs
@@ -1,3 +1,5 @@
+using Unity.Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,7 +8,6 @@ public class SpawnCharacterManager : MonoBehaviour
     public static SpawnCharacterManager Instance;
 
     [SerializeField] private GameObject character; // Reference to the existing character in the scene
-
     private void Awake()
     {
         if (Instance == null)
@@ -20,22 +21,42 @@ public class SpawnCharacterManager : MonoBehaviour
         }
     }
 
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            MoveCharacterToPosition(new Vector3(2.53f, 3.514f, 1.74f));
+        }
+    }
     public void MoveCharacterToPosition(Vector3 spawnPosition)
     {
         if (character != null)
         {
-            var controller = character.GetComponent<CharacterController>();
-            if (controller != null) controller.enabled = false;
+            GameObject character2 = GameObject.FindGameObjectWithTag("Player");
+            var controller = character2.GetComponent<CharacterController>();
+            controller.enabled = false;
 
-            character.transform.position = spawnPosition;
+            character2.transform.position = spawnPosition;
 
-            if (controller != null) controller.enabled = true;
+            controller.enabled = true;
 
             Debug.Log("Character moved to: " + character.transform.position);
         }
-        else
+        else if(character == null)
         {
             Debug.LogWarning("Character not assigned!");
         }
+    }
+
+    public void SpawnPositionOnStart(Vector3 spawnStartPosition)
+    {
+        GameObject character2 = GameObject.FindGameObjectWithTag("Player");
+        if(character2 != null)
+        {
+            MoveCharacterToPosition(spawnStartPosition);
+        }
+        Instantiate(character, spawnStartPosition, Quaternion.identity);
+
     }
 }
